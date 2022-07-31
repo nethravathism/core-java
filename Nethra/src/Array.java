0cc612d504175bcf33b89c62487cde4d6078e1c1
win10 <win10@DESKTOP-UCSRG6S>
2022-07-31 16:07:46 +0530

    Commit

---
+++ Nethra/src/Array.java
@@ -0,0 +1,21 @@
+
+public class Array 
+{
+
+	public static void main(String[] args)
+	{
+		char name[]=new char[7];//declaration
+		name[0]='N';//initialization
+		name[1]='E';
+		name[2]='T';
+		name[3]='H';
+		name[4]='R';
+		name[5]='A';
+				for(int i=0;i<=name.length-1;i++)
+		{
+			system.out.println(name[i]);
+	}
+	}
+}
+
+
