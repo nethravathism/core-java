249d0bbd90d520d447b5a4f07a57409dd2993c5e
win10 <win10@DESKTOP-UCSRG6S>
2022-07-31 16:51:32 +0530

    Commit

---
+++ Nethra/src/Nethrasm/Oops.java
@@ -0,0 +1,36 @@
+package Nethrasm;
+
+public class Oops {
+
+	public static void main(String[] args) {
+		int add(int a,int b)
+		{
+			int sum=a+b;
+			return sum;
+		}
+		float add(float a, float b)
+		{
+			return a+b;
+		}
+		float add(int a,float b,int c)
+		{
+			return a+b+c;
+		}
+		float add(float a,int b,int c)
+		{
+			return a+b+c;
+		}
+		
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			MethodOverloadingdemo obj= new MethodOverloadingdemo();
+			System.out.println(obj.add(10, 10));
+			System.out.println(obj.add(3.45f,5.64f));
+			System.out.println(obj.add(10, 22.23f,34));
+			System.out.println(obj.add(13.56f,39,47));
+			
+			
+	}
+
+}
